16b01fa037eda02118a35ca265bf714991af1d4b
Gabriel Naves da Silva <gabri.navess@gmail.com>
2016-07-13 16:17:01 -0300

    Created a scene loader class. Start page now loads the next page in build settings (right now it loads itself).

---
+++ Assets/Scripts/SceneLoading/SceneLoader.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    public void LoadScene(string scene) {
+        LoadSceneStatic(scene);
+    }
+
+    public void LoadScene(int sceneId) {
+        LoadSceneStatic(sceneId);
+    }
+
+    public void LoadNextScene() {
+        LoadNextSceneStatic();
+    }
+
+    static public void LoadSceneStatic(string scene) {
+        SceneManager.LoadScene(scene);
+    }
+
+    static public void LoadSceneStatic(int sceneId) {
+        SceneManager.LoadScene(sceneId);
+    }
+
+    static public void LoadNextSceneStatic() {
+        int nextSceneId = GetNextSceneId();
+        if (IsLastScene(nextSceneId))
+            nextSceneId = 0;
+        LoadSceneStatic(nextSceneId);
+    }
+
+    private static bool IsLastScene(int sceneId) {
+        return sceneId >= SceneManager.sceneCountInBuildSettings;
+    }
+
+    private static int GetNextSceneId() {
+        return SceneManager.GetActiveScene().buildIndex + 1;
+    }
+}
