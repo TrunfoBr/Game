c0ab2a4762ad073563ae73ebc06b3c80c0526648
Ícaro <mota.icaro@gmail.com>
2016-07-14 11:49:52 -0300

    Sending all tests done with the network.

---
+++ Assets/Scripts/Tests/Bullet.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        var hit = collision.gameObject;
+        var hitPlayer = hit.GetComponent<PlayerMove>();
+        if (hitPlayer != null)
+        {
+            var combat = hit.GetComponent<Combat>();
+            combat.TakeDamage(10);
+
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Tests/PlayerMove.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMove : NetworkBehaviour {
+    public GameObject bulletPrefab;
+
+    public override void OnStartLocalPlayer() {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+    void Update() {
+        if (!isLocalPlayer)
+            return;
+
+        var x = Input.GetAxis("Horizontal") * 0.1f;
+        var y = Input.GetAxis("Vertical") * 0.1f;
+
+        transform.Translate(x, y, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            // Command function is called from the client, but invoked on the server
+            CmdFire();
+        }
+    }
+
+    [Command]
+    void CmdFire()
+    {
+        // This [Command] code is run on the server!
+
+        // create the bullet object locally
+        var bullet = (GameObject)Instantiate(
+             bulletPrefab,
+             transform.position - transform.forward,
+             Quaternion.identity);
+
+        bullet.GetComponent<Rigidbody>().velocity = -new Vector3(1, 1, 1) * 4;
+
+        // spawn the bullet on the clients
+        NetworkServer.Spawn(bullet);
+
+        // when the bullet is destroyed on the server it will automaticaly be destroyed on clients
+        Destroy(bullet, 2.0f);
+    }
+}
