0ef4eca50497c3d19d7419a7a12f51aa53bceaf8
Ícaro <mota.icaro@gmail.com>
2016-07-14 15:56:08 -0300

    First implementation of the lobby is ready.

---
+++ Assets/LobbyPlayerBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class LobbyPlayerBehaviour : NetworkLobbyPlayer {
+
+    public override void OnClientEnterLobby() {
+        GetComponent<NetworkLobbyPlayer>().readyToBegin = true;
+    }
+
+}
