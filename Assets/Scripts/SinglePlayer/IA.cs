876f8f7420146e65776a5475d96234c62f6d3fb0
Ícaro <mota.icaro@gmail.com>
2016-07-15 15:27:25 -0300

    Implemented the dumbest AI ever. Period.

--- Assets/Scripts/SinglePlayer/IA.cs
+++ Assets/Scripts/SinglePlayer/IA.cs
@@ -3,8 +3,26 @@ using System.Collections;
 
 public class IA : SinglePlayer {
 
+    public float time = 5f;
+    private Timer timer;
+
+    void Start() {
+        timer = new Timer(time);
+    }
+
 	public override void AddCard(GameObject card) {
         base.AddCard(card);
         card.transform.localScale = new Vector3(0.6f, 0.6f, 0.6f);
+    }
+
+    protected override void PlayerUpdate() {
+        if (timer.IsOver()) {
+            hasDecidedAttribute = true;
+            attributeToPlay = Random.Range(0,4);
+            timer.Reset();
+        }
+        else {
+            timer.Run();
+        }
     }
 }
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer {
+    private float time;
+    private float elapsedTime = 0;
+
+    public Timer() {
+        Set(0);
+    }
+
+    public Timer(float time) {
+        Set(time);
+    }
+
+    public void Reset() {
+        elapsedTime = 0;
+    }
+
+    public void Run() {
+        elapsedTime = elapsedTime + Time.deltaTime;
+    }
+
+    public bool IsOver() {
+        return (elapsedTime >= time);
+    }
+
+    public void Set(float time) {
+        this.time = time;
+    }
+
+    public float GetElapsedTime() {
+        return elapsedTime;
+    }
+}
