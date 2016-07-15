eea994ccabf73f66c37e7ba8a558b0a2b6c41442
Ícaro <mota.icaro@gmail.com>
2016-07-15 12:23:58 -0300

    Listed all the players. With that, I could find out the players are getting the whole deck.

--- Assets/Scripts/MainDeck.cs
+++ Assets/Scripts/MainDeck.cs
@@ -6,18 +6,29 @@ using System.Collections.Generic;
 public class MainDeck : NetworkBehaviour {
 
     public static MainDeck Instance;
+    private int cardAmount = 0;
+    private bool deckReady = false;
 
     public override void OnStartServer() {
         var cards = CardList.Instance;
         for (int i = 0; i < cards.CardAmount; i++) {
+            cardAmount++;
             var card = cards.GetCard(i);
             card.transform.parent = transform;
             NetworkServer.Spawn(card);
         }
+        deckReady = true;
+    }
+
+    public bool stillHasCards() {
+        return cardAmount > 0;
     }
 
     public void GiveCardTo(GameObject player) {
-
+        if (!deckReady)
+            return;
+        Logger.LogError(this, player.ToString(), cardAmount.ToString());
+        cardAmount--;
     }
 
     void Awake() {
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,10 +3,24 @@ using UnityEngine.Networking;
 using System.Collections;
 
 public class Player : NetworkBehaviour {
+    
+    public static int numberOfPlayers = 0;
+
+    [SyncVar]
+    public int playerId;
+
+    void Awake() {
+        playerId = numberOfPlayers++;
+        gameObject.name = "Player" + playerId.ToString();
+    }
+
     void Update() {
         if (!isLocalPlayer)
             return;
 
-        MainDeck.Instance.GiveCardTo(gameObject);
+        var deck = MainDeck.Instance;
+        if (deck.stillHasCards()) {
+            deck.GiveCardTo(gameObject);
+        }
     }
 }
