09d91bfe16e9252f28fcf15318a94f4e32dcaa59
Ícaro <mota.icaro@gmail.com>
2016-07-15 11:10:36 -0300

    Implemented a deck that loads all cards, and is going to distribute them among players. Currently, the deck only spawns the cards.

---
+++ Assets/Scripts/MainDeck.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MainDeck : NetworkBehaviour {
+
+    public override void OnStartServer() {
+        var cards = CardList.Instance;
+        for (int i = 0; i < cards.CardAmount; i++) {
+            var card = cards.GetCard(i);
+            card.transform.parent = transform;
+            NetworkServer.Spawn(card);
+        }
+    }
+
+}
