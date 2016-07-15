97c3eb4a3dbdcb512afb9dc8ec9a3a22f9ce11e6
Ícaro <mota.icaro@gmail.com>
2016-07-15 11:43:15 -0300

    Players now access the deck, and can request for a card

--- Assets/Scripts/MainDeck.cs
+++ Assets/Scripts/MainDeck.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 
 public class MainDeck : NetworkBehaviour {
 
+    public static MainDeck Instance;
+
     public override void OnStartServer() {
         var cards = CardList.Instance;
         for (int i = 0; i < cards.CardAmount; i++) {
@@ -14,4 +16,14 @@ public class MainDeck : NetworkBehaviour {
         }
     }
 
+    public void GiveCardTo(GameObject player) {
+
+    }
+
+    void Awake() {
+        if (Instance == null)
+            Instance = this;
+        else
+            Logger.LogError(this, "Awake", "Instance already exists");
+    }
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class Player : NetworkBehaviour {
+    void Update() {
+        if (!isLocalPlayer)
+            return;
+
+        MainDeck.Instance.GiveCardTo(gameObject);
+    }
+}
