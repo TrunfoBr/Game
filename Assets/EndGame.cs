7e522958fae8475f8a693a56ffbeb03a4e14d23e
Ícaro <mota.icaro@gmail.com>
2016-07-15 15:49:51 -0300

    Implemented a class to find the winner of the game (not the round)

---
+++ Assets/EndGame.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EndGame : MonoBehaviour {
+
+    public static EndGame Instance;
+
+    private int numberOfAlivePlayers = 0;
+    private List<bool> alivePlayers = new List<bool>();
+    public List<GameObject> players;
+
+    public void NotifyDeath() {
+        for (int i=0; i<players.Count; i++) {
+            if(players[i].GetComponent<SinglePlayer>().my_deck.Count == 0 && alivePlayers[i] == true) {
+                alivePlayers[i] = false;
+                numberOfAlivePlayers--;
+            }
+        }
+        if (numberOfAlivePlayers == 1) {
+            FinishGame();
+        }
+    }
+
+    void Awake() {
+        if (Instance == null)
+            Instance = this;
+    }
+
+    void Start() {
+        foreach (GameObject player in players) {
+            alivePlayers.Add(true);
+            numberOfAlivePlayers++;
+        }
+    }
+
+    public void FinishGame() {
+        Debug.LogError("We have a victor");
+    }
+}
