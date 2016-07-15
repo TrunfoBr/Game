7bc36349b38d1914b9312422b41699f813eb3f76
Ícaro <mota.icaro@gmail.com>
2016-07-15 16:22:56 -0300

    Suffle deck at the beggining of the game

--- Assets/Scripts/SinglePlayer/Dealer.cs
+++ Assets/Scripts/SinglePlayer/Dealer.cs
@@ -9,10 +9,21 @@ public class Dealer : MonoBehaviour {
 
 	void Start () {
         deck = CardList.Instance;
+
+        int[] index = new int[deck.CardAmount];
+        for (int i=0; i<deck.CardAmount; i++) {
+            index[i] = i;
+        }
+        System.Array.Sort(index, RandomSort);
+
         for (int i=0; i<deck.CardAmount; i += players.Count) {
             for (int j=0; j<players.Count; j++) {
-                players[j].AddCard(deck.GetCard(i+j));
+                players[j].AddCard(deck.GetCard(index[i+j]));
             }
         }
 	}
+
+    public int RandomSort(int a, int b) {
+        return Random.Range(-1, 2);
+    }
 }
