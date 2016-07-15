8019ae9de2ecbe6f5aa3787ff45b0eaf609f72ad
Gabriel Naves da Silva <gabri.navess@gmail.com>
2016-07-15 14:37:08 -0300

    Fixed the mess we made on the multiplayer to single player transition

---
+++ Assets/Scripts/CardListLoadScreenMenu/CardListLoadScreenMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CardListLoadScreenMenu : MonoBehaviour {
+
+    public CardFactory cardFactory;
+    public string sceneToLoadOnEnd;
+    public Text text;
+
+    void Update() {
+        text.text = cardFactory.progress.ToString() + "%";
+        if (cardFactory.isDone)
+            SceneLoader.LoadSceneStatic(sceneToLoadOnEnd);
+    }
+}
