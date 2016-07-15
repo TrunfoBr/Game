5b865fb8c85e8ca5cd5723a489bda8962c0fdaa8
Gabriel Naves da Silva <gabri.navess@gmail.com>
2016-07-15 12:22:45 -0300

    Created the first version of a turn manager.

---
+++ Assets/Scripts/TurnManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class TurnManager : NetworkBehaviour {
+
+    public static TurnManager Instance { get; private set; }
+    public int currentPlayer { get; private set; }
+
+    private int playerAmount = 0;
+
+    public void PassTurn() {
+        if (++currentPlayer == playerAmount)
+            currentPlayer = 0;
+    }
+
+    public void RegisterPlayer() {
+        playerAmount++;
+    }
+    
+    public override void OnStartServer() {
+        currentPlayer = 0;
+    }
+
+    void Awake() {
+        if (Instance == null)
+            Instance = this;
+        else
+            Logger.LogError(this, "Awake", "Instance already exists");
+    }
+}
